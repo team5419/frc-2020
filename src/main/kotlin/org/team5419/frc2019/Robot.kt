a9982e4f5b7b54700c313a67f85c4ea57e7a72b1
Andy Cate <and164v@gmail.com>
2019-05-02 16:46:19 -0700

    Initial commit by cookiecutter

---
+++ src/main/kotlin/org/team5419/frc2019/Robot.kt
@@ -0,0 +1,9 @@
+package org.team5419.frc2019
+
+import edu.wpi.first.wpilibj.TimedRobot
+
+class Robot : TimedRobot() {
+    override fun robotInit() {
+        println("Hello World from Kotlin!")
+    }
+}
