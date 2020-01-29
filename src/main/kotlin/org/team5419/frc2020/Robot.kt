83a3dec5522dfb5ef7ec33bf5a5aa32fbba1ba78
JudeKratzer <tdujgamer@gmail.com>
2020-01-28 18:19:58 -0800

    shuffleboard

--- src/main/kotlin/org/team5419/frc2020/Robot.kt
+++ src/main/kotlin/org/team5419/frc2020/Robot.kt
@@ -11,6 +11,8 @@ import org.team5419.frc2020.subsystems.*
 import org.team5419.frc2020.auto.generateRoutines
 import org.team5419.fault.BerkeliumRobot
 import org.team5419.fault.math.units.seconds
+import org.team5419.fault.math.units.derived.radians
+import org.team5419.fault.math.units.derived.velocity
 import org.team5419.fault.math.geometry.Pose2d
 import org.team5419.fault.auto.Routine
 
@@ -40,6 +42,7 @@ class Robot : BerkeliumRobot(0.05.seconds) {
 
     override fun robotPeriodic() {
         Shuffleboard.update()
+        Shooger.shoog(shooterVelocity.getDouble(6000.0).radians.velocity)
     }
 
     override fun disabledInit() {
