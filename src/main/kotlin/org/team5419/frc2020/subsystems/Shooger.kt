037e1d5369c0e123b36ea477778e13c4932b3085
JudeKratzer <tdujgamer@gmail.com>
2020-01-30 16:46:55 -0800

    shooter test

--- src/main/kotlin/org/team5419/frc2020/subsystems/Shooger.kt
+++ src/main/kotlin/org/team5419/frc2020/subsystems/Shooger.kt
@@ -32,7 +32,8 @@ object Shooger : Subsystem("Shooger") {
             config_kD(0, HoodConstants.kD)
         }
 
-        masterMotor.talonSRX.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative)
+        masterMotor.talonSRX.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative)\
+        masterMotor.talonSRX.setPhase(false)
 
         slaveMotor1.follow(masterMotor)
         slaveMotor2.follow(masterMotor)
@@ -56,8 +57,13 @@ object Shooger : Subsystem("Shooger") {
         return velocity * ShoogerConstants.kV
     }
 
+    var setpoint = 0.0
+
     public fun shoog (shoogVelocity : SIUnit<AngularVelocity>) {
-        masterMotor.setVoltage(calculateFeedforward(shoogVelocity))
+        setpoint = calculateFeedforward(shoogVelocity)
+
+        this.setpoint = Constants.flywheel.fromNativeUnitVelocity(setpoint)
+        masterMotor.setVoltage(setpoint)
     }
 
     public fun setPercent (percent: Double) {
