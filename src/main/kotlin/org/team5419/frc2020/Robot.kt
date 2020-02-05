406fbc5cb72ce3f6a8f1e09d1c4e420d49e6bc7d
Nicholas Underwood <nachos.underwood@gmail.com>
2020-02-04 17:01:56 -0800

    shuffleboard + wpi pid

--- src/main/kotlin/org/team5419/frc2020/Robot.kt
+++ src/main/kotlin/org/team5419/frc2020/Robot.kt
@@ -1,11 +1,11 @@
 package org.team5419.frc2020
 
 import edu.wpi.first.wpilibj.XboxController
-import edu.wpi.first.wpilibj.shuffleboard.*
-import edu.wpi.first.networktables.NetworkTableEntry
+
 import org.team5419.frc2020.controllers.TeleopController
 import org.team5419.frc2020.controllers.AutoController
 import org.team5419.frc2020.subsystems.*
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
 import org.team5419.fault.BerkeliumRobot
 import org.team5419.fault.math.units.seconds
 import org.team5419.fault.math.geometry.Pose2d
@@ -17,13 +17,11 @@ class Robot : BerkeliumRobot(0.05.seconds) {
     private val mDriver: XboxController
     private val mCodriver: XboxController
     private val teleopController: TeleopController
-    private val tab: ShuffleboardTab
 
     init {
         mDriver = XboxController(0)
         mCodriver = XboxController(1)
         teleopController = TeleopController(mDriver, mCodriver)
-        tab = Shuffleboard.getTab("Auto")
 
         // subsystem manager
         // +Drivetrain
@@ -31,12 +29,6 @@ class Robot : BerkeliumRobot(0.05.seconds) {
         // +Intake
         // +Shooger
         // +Spinner
-
-        tab.addBoolean("Found Target", { Vision.targetFound })
-        tab.addNumber("Horizontal Offset", { Vision.horizontalOffset })
-        tab.addNumber("Vertical Offset", { Vision.verticalOffset })
-        tab.addNumber("Target Area", { Vision.targetArea })
-        tab.addNumber("Target Skew", { Vision.targetSkew })
     }
 
     override fun robotInit() {
--- src/main/kotlin/org/team5419/frc2020/subsystems/Vision.kt
+++ src/main/kotlin/org/team5419/frc2020/subsystems/Vision.kt
@@ -5,6 +5,9 @@ import org.team5419.fault.hardware.Limelight
 import org.team5419.fault.math.units.*
 import org.team5419.fault.subsystems.Subsystem
 import org.team5419.frc2020.VisionConstants
+import edu.wpi.first.wpilibj.shuffleboard.*
+import edu.wpi.first.networktables.NetworkTableEntry
+import edu.wpi.first.wpilibj.controller.PIDController
 
 object Vision: Limelight (
     networkTableName = "limelight",
@@ -13,31 +16,28 @@ object Vision: Limelight (
     mCameraHeight = VisionConstants.kCameraHeight,
     mCameraAngle = VisionConstants.kCameraAngle
 ) {
+    const val kP: Double = 1.0/30.0
+    const val kI: Double = 0.0
+    const val kD: Double = 0.0
 
+    // private val pidEntry : NetworkTableEntry
+    private val tab: ShuffleboardTab
+    private val controller: PIDController = PIDController(kP, kI, kI)
+    private var output: Double = 0.0
 
-    // const val kI: Double = 1.0
-    // const val kP: Double = 0.0
-    // const val kD: Double = 0.0
+    init{
+        tab = Shuffleboard.getTab("Auto")
 
-    // var lastOffset: Double
-    // var output: Double = 0.0
-    // var integral: Double = 0.0
-    // var lastTime: Double = 0.0
-    // val timer: Timer = Timer()
+        tab.add("Vision PID", controller).withWidget(BuiltInWidgets.kPIDCommand)
+        tab.addBoolean("Found Target", { targetFound })
+        tab.addNumber("Horizontal Offset", { horizontalOffset })
+        tab.addNumber("Vertical Offset", { verticalOffset })
+        tab.addNumber("Target Area", { targetArea })
+        tab.addNumber("Target Skew", { targetSkew })
+    }
 
-    // fun sigmoid(x : Double) : Double = 1 / ( Math.exp(-x) + 1 )
-
-    // init{
-    //     Timer.start()
-    //     lastOffset = horizontalOffset
-    // }
-
-    // fun periodic() {
-    //     var e = horizontalOffset
-    //     var time = timer.get()
-    //     integral += e
-    //     output = kP * e + kD * (e - lastOffset) / (time - lastTime) + kI * integral
-    //     output = sigmoid(output)
-    //     Drivetrain.setPercent(output, -output)
-    // }
+    fun periodic() {
+        output = controller.calculate(horizontalOffset)
+        Drivetrain.setPercent(output, -output)
+    }
 }
