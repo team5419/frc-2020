ea08dc91d8cb63236b1603c8155e0cb1fe0dbcf4
Nicholas Underwood <nachos.underwood@gmail.com>
2020-02-04 17:35:27 -0800

    shuffleboard not working

--- src/main/kotlin/org/team5419/frc2020/Robot.kt
+++ src/main/kotlin/org/team5419/frc2020/Robot.kt
@@ -4,8 +4,8 @@ import edu.wpi.first.wpilibj.XboxController
 
 import org.team5419.frc2020.controllers.TeleopController
 import org.team5419.frc2020.controllers.AutoController
-import org.team5419.frc2020.subsystems.*
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
+import org.team5419.frc2020.subsystems.Vision
+import edu.wpi.first.wpilibj.shuffleboard.*
 import org.team5419.fault.BerkeliumRobot
 import org.team5419.fault.math.units.seconds
 import org.team5419.fault.math.geometry.Pose2d
@@ -17,11 +17,21 @@ class Robot : BerkeliumRobot(0.05.seconds) {
     private val mDriver: XboxController
     private val mCodriver: XboxController
     private val teleopController: TeleopController
+    private val tab: ShuffleboardTab
+
 
     init {
         mDriver = XboxController(0)
         mCodriver = XboxController(1)
         teleopController = TeleopController(mDriver, mCodriver)
+        tab = Shuffleboard.getTab("Vision")
+        println("Init")
+
+        tab.add("Vision PID", Vision.controller).withWidget(BuiltInWidgets.kPIDCommand)
+        tab.addBoolean("Found Target", { Vision.targetFound })
+        tab.addNumber("PID Output", { Vision.output })
+
+
 
         // subsystem manager
         // +Drivetrain
@@ -37,6 +47,7 @@ class Robot : BerkeliumRobot(0.05.seconds) {
 
     override fun robotPeriodic() {
         // Drivetrain.periodic()
+        Vision.periodic()
         Shuffleboard.update()
     }
 
