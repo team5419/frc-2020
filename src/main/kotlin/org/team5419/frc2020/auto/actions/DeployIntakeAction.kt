fcb6e5c0635e613c1a01dd9d3d59d95d8ece65ba
Nicholas Underwood <nachos.underwood@gmail.com>
2020-02-27 20:19:17 -0800

    better actions

---
+++ src/main/kotlin/org/team5419/frc2020/auto/actions/DeployIntakeAction.kt
@@ -0,0 +1,18 @@
+package org.team5419.frc2020.auto.actions
+
+import org.team5419.frc2020.subsystems.Intake.DeployMode
+import org.team5419.frc2020.subsystems.Intake
+import org.team5419.fault.auto.Action
+
+public class DeployIntakeAction(): ConfigDeployAction(DeployMode.DEPLOY)
+public class RetractIntakeAction(): ConfigDeployAction(DeployMode.STORE)
+public class DisableIntakeDeployAction(): ConfigDeployAction(DeployMode.OFF)
+
+open class ConfigDeployAction(val deploy: DeployMode): Action(){
+    override fun start(){
+        Intake.deployMode = deploy
+    }
+
+    override fun next() = true
+
+}
