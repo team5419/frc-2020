bcb4a5cce1bf130033b7a9e262cad05686d8c7f0
Nicholas Underwood <nachos.underwood@gmail.com>
2020-02-28 23:16:13 -0800

    hood actions

--- src/main/kotlin/org/team5419/frc2020/auto/actions/HoodAction.kt
+++ src/main/kotlin/org/team5419/frc2020/auto/actions/HoodAction.kt
@@ -5,7 +5,15 @@ import org.team5419.frc2020.subsystems.Hood.HoodPosititions
 import org.team5419.frc2020.subsystems.Drivetrain
 import org.team5419.fault.auto.Action
 
-class HoodAction(val pos: HoodPosititions) : Action() {
+public class FarHoodAction() : HoodAction(HoodPosititions.FAR)
+public class TrussHoodAction() : HoodAction(HoodPosititions.TRUSS)
+public class AutoHoodAction(): HoodAction(HoodPosititions.AUTO)
+public class CloseHoodAction(): HoodAction(HoodPosititions.CLOSE)
+public class RetractHoodAction(): HoodAction(HoodPosititions.CLOSE)
+
+
+
+open class HoodAction(val pos: HoodPosititions) : Action() {
     override public fun start() {
         Hood.goto(pos)
     }
