99ce2ddfd0a198a8222374499dbf88fdb2fc2505
Nicholas Underwood <nachos.underwood@gmail.com>
2020-03-03 19:29:09 -0800

    builds, unsure of units

--- src/main/kotlin/org/team5419/frc2020/auto/actions/FollowAction.kt
+++ src/main/kotlin/org/team5419/frc2020/auto/actions/FollowAction.kt
@@ -4,21 +4,72 @@ import org.team5419.frc2020.subsystems.Drivetrain
 import com.ctre.phoenix.motion.*
 import com.ctre.phoenix.motorcontrol.*
 import org.team5419.fault.auto.Action
+import org.team5419.frc2020.RobotConstants
+import org.team5419.frc2020.DriveConstants
+import org.team5419.fault.math.geometry.Pose2d
+import jaci.pathfinder.Pathfinder
+import jaci.pathfinder.Waypoint
+import jaci.pathfinder.Trajectory
+import jaci.pathfinder.modifiers.TankModifier
 
-class FollowAction : Action() {
+class FollowAction(
+    waypoints: Array<Pose2d>,
+    maxVelocity: Double = DriveConstants.MaxVelocity.value,
+    maxAcceleration: Double = DriveConstants.MaxAcceleration.value,
+    maxJerk: Double = 5.0 //Default value provided
+) : Action() {
     val minPoints = 5
 
-    val trajectoryPoints = BufferedTrajectoryPointStream()
+    val leftTrajectoryPoints = BufferedTrajectoryPointStream()
+    val rightTrajectoryPoints = BufferedTrajectoryPointStream()
 
     init {
-        trajectoryPoints.Write(arrayOf<TrajectoryPoint>(
 
-        ))
+        if(waypoints.size < minPoints) println("Not enough points")
+        val jaciArray: Array<Waypoint> = Array<Waypoint> (waypoints.size) { i ->
+            Waypoint(
+                waypoints.get(i).translation.x.value,
+                waypoints.get(i).translation.y.value,
+                waypoints.get(i).rotation.degree
+            )
+        }
+        val config: Trajectory.Config = Trajectory.Config(
+            Trajectory.FitMethod.HERMITE_QUINTIC,
+            Trajectory.Config.SAMPLES_HIGH,
+            0.01,
+            maxVelocity,
+            maxAcceleration,
+            maxJerk
+        )
+        val source: Trajectory = Pathfinder.generate( jaciArray, config )
+        val modifier: TankModifier = TankModifier( source )
+        modifier.modify( RobotConstants.Width.value )
+        val rightTrajectory: Trajectory = modifier.getRightTrajectory()
+        val leftTrajectory: Trajectory = modifier.getLeftTrajectory()
+
+
+
+        rightTrajectoryPoints.Write(trajectoryToPoints(rightTrajectory))
+        leftTrajectoryPoints.Write(trajectoryToPoints(leftTrajectory))
+    }
+
+    private fun trajectoryToPoints(traj: Trajectory): Array<TrajectoryPoint> {
+
+        return Array<TrajectoryPoint>(traj.segments.size) { i ->
+            val segement = traj.segments[i]
+            val point = TrajectoryPoint()
+            point.position = segement.position
+            point.velocity = segement.velocity
+            point.headingDeg = segement.heading
+            point.isLastPoint = i == traj.segments.size - 1
+            point
+        }
+
     }
 
     override fun start() {
-        Drivetrain.leftMasterMotor.startMotionProfile(trajectoryPoints, minPoints, ControlMode.MotionProfile)
-        Drivetrain.rightMasterMotor.startMotionProfile(trajectoryPoints, minPoints, ControlMode.MotionProfile)
+        Drivetrain.leftMasterMotor.startMotionProfile(leftTrajectoryPoints, minPoints, ControlMode.MotionProfile)
+        Drivetrain.rightMasterMotor.startMotionProfile(rightTrajectoryPoints, minPoints, ControlMode.MotionProfile)
     }
 
     override fun update() {
